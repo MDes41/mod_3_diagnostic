9895974b1c3da50ebc4fe7cf2cc860a9439dda6e
Matt D <MDes41@users.noreply.github.com>
2016-12-14 11:10:24 -0700

    Stations formatted

--- app/models/stations.rb
+++ app/models/stations.rb
@@ -2,11 +2,11 @@ class Stations
 	attr_reader :distance, :station_name, :street_address, :access_days_time, :fuel_type_code
 
 	def initialize(station)
-		@distance = station[:distance]
+		@distance = format_distance(station)
 		@station_name = station[:station_name]
-		@street_address = station[:street_address]
+		@street_address = address(station)
 		@access_days_time = station[:access_days_time]
-		@fuel_type_code = station[:fuel_type_code]
+		@fuel_type_code = fuel_type(station)
 	end
 
 	def self.get_nearest_stations(zip)
@@ -15,4 +15,16 @@ class Stations
 		end
 	end
 
+	def address(station)
+		"#{station[:street_address]} #{station[:city]}, #{station[:state]} #{station[:zip]}"
+	end
+
+	def fuel_type(station)
+		fuel_types = { 'ELEC' => 'Electric', 'LPG' => 'Liquefied Petroleum Gas (Propane)'  }
+		fuel_types[station[:fuel_type_code]]
+	end
+
+	def format_distance(station)
+		"#{station[:distance]} Miles"
+	end
 end
--- spec/models/stations_spec.rb
+++ spec/models/stations_spec.rb
@@ -68,11 +68,11 @@ describe 'Stations Model' do
 				
 			station = Stations.new(JSON.parse(response, symbolize_names: true))
 
-			expect(station.distance).to eq('0.3117')
+			expect(station.distance).to eq('0.3117 Miles')
 			expect(station.station_name).to eq('UDR')
-			expect(station.street_address).to eq("800 Acoma St")
+			expect(station.street_address).to eq("800 Acoma St Denver, CO 80204")
 			expect(station.access_days_time).to eq("24 hours daily")
-			expect(station.fuel_type_code).to eq("ELEC")
+			expect(station.fuel_type_code).to eq("Electric")
 		end
 	end 
 end
